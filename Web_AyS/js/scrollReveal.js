a73f93307dd07d34555e148fdb0a98f58316d7ce
Guido <guidoagustinbeltrame@gmail.com>
2019-07-14 17:38:24 -0300

    Actualizando Version Web.

---
+++ Web_AyS/js/scrollReveal.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal({reset:true});
+
+var servicio = {
+  distance: '100%',
+  origin:'bottom',
+  duration: 1000
+}
+
+sr.reveal('.containerServicio', servicio);
+sr.reveal('.row',{
+  duration: 2000,
+});
